1a345e098626599ef0c7a398b85bb56334602239
carlsolon <130752815+carlsolon@users.noreply.github.com>
2024-11-26 09:30:15 +0700

    Update script.js

--- script.js
+++ script.js
@@ -5,12 +5,12 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Aaaaa, isahan nasad ka Sy hahahaha";
+  question.innerHTML = "Mag pa kiss raman diay hahah puhon love hehhe";
   gif.src =
     "https://raw.githubusercontent.com/DzarelDeveloper/Img/main/gif.webp";
 });
 
-noBtn.addEventListener("mouseover", () => {
+noBtn.addEventListener("click", () => {
   const noBtnRect = noBtn.getBoundingClientRect();
   const maxX = window.innerWidth - noBtnRect.width;
   const maxY = window.innerHeight - noBtnRect.height;
